bbf06a332e3f73c1795626593c17fb7bf178620c
TWARIQABDUL <tariqazizibabu@gmail.com>
2023-03-31 16:17:10 +0200

    memory alocation

---
+++ ownership/target/debug/deps/ownership-8804ca5659399605.d
@@ -0,0 +1,5 @@
+c:\Users\high\Desktop\rust\ownership\target\debug\deps\ownership-8804ca5659399605.rmeta: src\main.rs
+
+c:\Users\high\Desktop\rust\ownership\target\debug\deps\ownership-8804ca5659399605.d: src\main.rs
+
+src\main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+C:\Users\high\Desktop\rust\ownership\target\debug\ownership.exe: C:\Users\high\Desktop\rust\ownership\src\main.rs
