07ad150ed5bf76274bebe954e5f460eb85d46717
TWARIQABDUL <69117049+TWARIQABDUL@users.noreply.github.com>
2023-04-12 10:22:13 +0200

    Delete enum-data/target directory

--- enum-data/target/debug/deps/enum_data-d6c0ffafb49759eb.d
+++
@@ -1,5 +0,0 @@
-c:\Users\high\Desktop\rust\enum-data\target\debug\deps\enum_data-d6c0ffafb49759eb.rmeta: src\main.rs
-
-c:\Users\high\Desktop\rust\enum-data\target\debug\deps\enum_data-d6c0ffafb49759eb.d: src\main.rs
-
-src\main.rs:
--- enum-data/target/debug/enum-data.d
+++
@@ -1,1 +0,0 @@
-C:\Users\high\Desktop\rust\enum-data\target\debug\enum-data.exe: C:\Users\high\Desktop\rust\enum-data\src\main.rs
