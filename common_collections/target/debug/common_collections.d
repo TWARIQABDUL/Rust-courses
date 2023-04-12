914881c553328a9e51c64784b5fe0a887ef90479
TWARIQABDUL <69117049+TWARIQABDUL@users.noreply.github.com>
2023-04-12 10:31:41 +0200

    Delete common_collections directory

--- common_collections/target/debug/common_collections.d
+++
@@ -1,1 +0,0 @@
-C:\Users\high\Desktop\rust\common_collections\target\debug\common_collections.exe: C:\Users\high\Desktop\rust\common_collections\src\main.rs
--- common_collections/target/debug/deps/common_collections.d
+++
@@ -1,5 +0,0 @@
-C:\Users\high\Desktop\rust\common_collections\target\debug\deps\common_collections.exe: src\main.rs
-
-C:\Users\high\Desktop\rust\common_collections\target\debug\deps\common_collections.d: src\main.rs
-
-src\main.rs:
