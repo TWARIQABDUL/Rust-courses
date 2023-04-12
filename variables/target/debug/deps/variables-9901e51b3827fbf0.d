1a6f3fda392adb133ee411a8023bbc648ea96bdb
TWARIQABDUL <69117049+TWARIQABDUL@users.noreply.github.com>
2023-04-12 10:25:21 +0200

    Delete variables/target directory

--- variables/target/debug/deps/variables-9901e51b3827fbf0.d
+++
@@ -1,5 +0,0 @@
-c:\Users\high\Desktop\rust\variables\target\debug\deps\variables-9901e51b3827fbf0.rmeta: src\main.rs
-
-c:\Users\high\Desktop\rust\variables\target\debug\deps\variables-9901e51b3827fbf0.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/deps/variables-d60ea5a60bbd29f2.d
+++
@@ -1,5 +0,0 @@
-c:\Users\high\Desktop\rust\variables\target\debug\deps\variables-d60ea5a60bbd29f2.rmeta: src\main.rs
-
-c:\Users\high\Desktop\rust\variables\target\debug\deps\variables-d60ea5a60bbd29f2.d: src\main.rs
-
-src\main.rs:
