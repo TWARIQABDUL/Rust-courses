8bea0ca9784236d9faca56b8607b0e343db50218
TWARIQABDUL <tariqazizibabu@gmail.com>
2023-04-12 10:29:50 +0200

    common collection 2

---
+++ common_collections/target/debug/common_collections.d
@@ -0,0 +1,1 @@
+C:\Users\high\Desktop\rust\common_collections\target\debug\common_collections.exe: C:\Users\high\Desktop\rust\common_collections\src\main.rs
---
+++ common_collections/target/debug/deps/common_collections.d
@@ -0,0 +1,5 @@
+C:\Users\high\Desktop\rust\common_collections\target\debug\deps\common_collections.exe: src\main.rs
+
+C:\Users\high\Desktop\rust\common_collections\target\debug\deps\common_collections.d: src\main.rs
+
+src\main.rs:
