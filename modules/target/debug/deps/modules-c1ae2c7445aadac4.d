3e50843df3cb4eb7a6f972d016363e3a2f061994
TWARIQABDUL <69117049+TWARIQABDUL@users.noreply.github.com>
2023-04-13 17:19:11 +0200

    Delete modules/target directory

--- modules/target/debug/deps/modules-c1ae2c7445aadac4.d
+++
@@ -1,5 +0,0 @@
-c:\Users\high\Desktop\rust\modules\target\debug\deps\modules-c1ae2c7445aadac4.rmeta: src\lib.rs
-
-c:\Users\high\Desktop\rust\modules\target\debug\deps\modules-c1ae2c7445aadac4.d: src\lib.rs
-
-src\lib.rs:
--- modules/target/debug/libmodules.d
+++
@@ -1,1 +0,0 @@
-C:\Users\high\Desktop\rust\modules\target\debug\libmodules.rlib: C:\Users\high\Desktop\rust\modules\src\lib.rs
